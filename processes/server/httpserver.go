7b8bd73c00531c406ea80b39a375618b17a9cf8b
刘若愚 <liuruoyu@xunlei.com>
2018-03-13 13:50:22 +0800

    move out of aws

--- processes/server/httpserver.go
+++ processes/server/httpserver.go
@@ -46,8 +46,8 @@ func main() {
 
 	router.GET("/yoyo/suggest", server.Suggest)
 	router.GET("/yoyo/search", server.Search)
-	router.POST("/yoyo/:infohash/download", server.UpdateDownloadCount)
 	router.POST("/yoyo/update", server.UpdateMetaInfo)
+	router.POST("/yoyo/:infohash/download", server.UpdateDownloadCount)
 
 	endless.ListenAndServe(address, router)
 	os.Exit(0)
