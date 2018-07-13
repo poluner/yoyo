e705860d20603dcb20e4775e929d0e54551881ec
刘若愚 <liuruoyu@xunlei.com>
2018-07-13 13:07:23 +0800

    whatsapp

--- server/whatsapp.go
+++ server/whatsapp.go
@@ -4,7 +4,7 @@ package server
 
 func (p *listParam) WhatsApp() (result []WhatsApp, err error) {
 	cmdResult := redisConn.LRange(
-		whatsAppRedisList, int64(p.Offset), int64(p.Offset + p.Limit))
+		whatsAppRedisList, int64(p.Offset), int64(p.Offset + p.Limit - 1))
 	resourceIds := cmdResult.Val()
 	result, err = QueryWhatsapp(p.ctx, resourceIds)
 	return
